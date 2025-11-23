f38142d99a3067dbbf1ad302df1fb757a38df182
wyt <114574109+wangyutong138168@users.noreply.github.com>
2025-11-23 09:21:17 +0700

    Add files via upload

---
+++ Array & String/two_sum_prac_003.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def twoSum(s,number, target)
+        seen = {}
+
+        if i, in nums enumerate(num): 
+            need = target - num
+
+            if need in seen:
+                return [seen[need],i]
