204c4827aa352f528f6e645a1e0f80eabda97cf1
wyt <114574109+wangyutong138168@users.noreply.github.com>
2025-11-30 14:48:57 +0700

    Update 002_prac_1071_greatest_common_divisory_of_strings.py

--- Array & String/002_prac_1071_greatest_common_divisory_of_strings.py
+++ Array & String/002_prac_1071_greatest_common_divisory_of_strings.py
@@ -3,7 +3,3 @@ result = []
     for a,b in zip(word1,word2):
         result.append(a)
         result.append(b)
-
-    result.append(word1[len(result)//2:])
-    result.append(word2[len(result//2:)])
-    
