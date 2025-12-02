5c64c8e1c4bf09ef8b539bc1d68200ea718edac4
wyt <114574109+wangyutong138168@users.noreply.github.com>
2025-12-02 21:21:08 +0700

    Add files via upload

---
+++ Array & String/004_prac_1071_greatest_common_divisory_of_strings.py
@@ -0,0 +1,9 @@
+result = []
+    for a,b in zip(word1,word2):
+        result.append(a)
+        result.append(b)
+
+    result.append(word1[len(result//2:)])
+    result.append(word2[len(result//2:)])
+
+    return ''.join(result)
