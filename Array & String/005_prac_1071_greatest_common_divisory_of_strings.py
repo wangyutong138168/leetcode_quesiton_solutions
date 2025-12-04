0f0596e96167c6fc21e5f7e7b63bc5d80e7c59d3
wyt <114574109+wangyutong138168@users.noreply.github.com>
2025-12-04 18:32:31 +0700

    Add files via upload

---
+++ Array & String/005_prac_1071_greatest_common_divisory_of_strings.py
@@ -0,0 +1,12 @@
+word1 = "backpack"
+word2 = "notebook"
+result = []
+
+for a, b in zip(word1, word2):
+    result.append(a)
+    result.append(b)
+
+result.append(word1[len(result) // 2:])
+result.append(word2[len(result) // 2:])
+
+print("".join(result))
