814186428dac577d0019621869258e32da6b86b7
wyt <114574109+wangyutong138168@users.noreply.github.com>
2025-12-15 19:22:11 +0700

    Add files via upload

---
+++ Array & String/prac_002_1768_merge_strings_alternately.py
@@ -0,0 +1,6 @@
+class solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        res = ""
+        for a,b in zip(word1,word2):
+            res+=a,b
+        return res + word1[len(word2):] + word2[len(word1):]
