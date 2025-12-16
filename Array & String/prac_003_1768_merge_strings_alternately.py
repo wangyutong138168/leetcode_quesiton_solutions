cc0d8bcf2a4392b9feb4e017667cad1c72ae368d
wyt <114574109+wangyutong138168@users.noreply.github.com>
2025-12-16 21:34:17 +0700

    Add files via upload

---
+++ Array & String/prac_003_1768_merge_strings_alternately.py
@@ -0,0 +1,9 @@
+class Solution:
+    def mergeAlternate(self, word1: str, word2: str) -> str:
+        res = ""
+        for a, b in zip(word1, word2):
+            res += a + b
+        return res + word1[len(word2):] + word2[len(word1):]
+
+s = Solution()
+print(s.mergeAlternate("bubble", "gum"))
