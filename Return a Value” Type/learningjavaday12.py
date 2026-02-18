9bd42c77760a8d6e7d568e577c50fa59781151a0
wyt <114574109+wangyutong138168@users.noreply.github.com>
2026-02-18 21:38:25 +0700

    Add files via upload

---
+++ Return a Value” Type/learningjavaday12.py
@@ -0,0 +1,12 @@
+class Stock {
+    String ticker;
+    double price;
+
+    public Stock(String t, double p) {
+        ticker = t;
+        price = p;
+    }
+}
+
+
+Stock s = new Stock("AAPL", 185.0);
