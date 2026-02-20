9f9d976dc8663435ecfe21d990b0ca0350fec160
wyt <114574109+wangyutong138168@users.noreply.github.com>
2026-02-20 16:38:22 +0700

    Add files via upload

---
+++ Return a Value” Type/learningjavaday13.py
@@ -0,0 +1,12 @@
+public class GoldAsset {
+    private double price; 
+
+    public double getPrice() {
+        return price;
+    }
+
+
+    public void setPrice(double p) {
+        if (p > 0) price = p; 
+    }
+}
