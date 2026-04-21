6dc1376a8db60bbce8357b95c15974e6cf88f296
wyt <114574109+wangyutong138168@users.noreply.github.com>
2026-04-21 20:13:46 +0700

    Add files via upload

---
+++ Array & String/fronttimes_java.py
@@ -0,0 +1,6 @@
+public String frontTimes(String str, int n) {
+  String front = str.length() < 3 ? str : str.substring(0, 3);
+  String result = "";
+  for (int i = 0; i < n; i++) result += front;
+  return result;
+}
