a41263cbc5aa104987a5c77bb95eaa52cd481f06
wyt <114574109+wangyutong138168@users.noreply.github.com>
2026-05-02 18:20:54 +0700

    Add files via upload

---
+++ Array & String/stringX_java.py
@@ -0,0 +1,7 @@
+public String stringX(String str) {
+  String result = "";
+  for (int i = 0; i < str.length(); i++) {
+    if ((i == 0 || i == str.length() - 1) || str.charAt(i) != 'x') result += str.charAt(i);
+  }
+  return result;
+}
