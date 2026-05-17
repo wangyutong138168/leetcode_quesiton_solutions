681acf1c0eaf05688a41382aa5a5fb621a43f323
wyt <114574109+wangyutong138168@users.noreply.github.com>
2026-05-17 14:07:12 +0700

    Add files via upload

---
+++ Array & String/startword.py
@@ -0,0 +1,7 @@
+public String startWord(String str, String word) {
+  if (str.length() >= word.length() &&
+      str.substring(1, word.length()).equals(word.substring(1))) {
+    return str.substring(0, word.length());
+  }
+  return "";
+}
