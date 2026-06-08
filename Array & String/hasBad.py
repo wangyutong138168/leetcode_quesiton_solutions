44ce2e7fa56864028550576d56fa90a29486058b
wyt <114574109+wangyutong138168@users.noreply.github.com>
2026-06-08 16:02:10 +0700

    Create hasBad.py

---
+++ Array & String/hasBad.py
@@ -0,0 +1,5 @@
+public boolean hasBad(String str) {
+  if (str.length() >= 3 && str.substring(0, 3).equals("bad")) return true;
+  if (str.length() >= 4 && str.substring(1, 4).equals("bad")) return true;
+  return false;
+}
