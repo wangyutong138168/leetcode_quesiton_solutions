bee41f0c172afe5169793734f98705a7d4c8501e
wyt <114574109+wangyutong138168@users.noreply.github.com>
2026-07-01 16:58:08 +0700

    Create towchar_java.java

---
+++ Array & String/towchar_java.java
@@ -0,0 +1,6 @@
+public String twoChar(String str, int index) {
+    if (index < 0 || index > str.length() - 2) {
+        return str.substring(0, 2);
+    }
+    return str.substring(index, index + 2);
+}
