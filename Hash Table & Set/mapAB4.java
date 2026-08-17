8c5c6edcd540f54d49e76b43c28ab41cffb2dfa6
wyt <114574109+wangyutong138168@users.noreply.github.com>
2026-08-17 17:45:59 +0700

    Create mapAB4.java

---
+++ Hash Table & Set/mapAB4.java
@@ -0,0 +1,11 @@
+public Map<String, String> mapAB4(Map<String, String> map) {
+    if (map.containsKey("a") && map.containsKey("b")) {
+        if (map.get("a").length() > map.get("b").length()) map.put("c", map.get("a"));
+        else if (map.get("b").length() > map.get("a").length()) map.put("c", map.get("b"));
+        else {
+            map.put("a", "");
+            map.put("b", "");
+        }
+    }
+    return map;
+}
